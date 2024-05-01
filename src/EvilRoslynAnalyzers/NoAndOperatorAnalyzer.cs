1271b0f856e6ec543b91ce93d558e85a55ed14f3
WilliamBoulton <will@willboulton.com>
2024-05-01 22:20:54 +0100

    update description

--- src/EvilRoslynAnalyzers/NoAndOperatorAnalyzer.cs
+++ src/EvilRoslynAnalyzers/NoAndOperatorAnalyzer.cs
@@ -12,7 +12,7 @@ public class NoAndOperatorAnalyzer : DiagnosticAnalyzer
     
     private static readonly string Title = "AndAlso operator is forbidden";
     private static readonly string MessageFormat = "Using the && operator is not allowed";
-    private static readonly string Description = "All boolean and operator (&&) usages are forbidden in this codebase. Try using and expressions instead.";
+    private static readonly string Description = "All boolean and operator (&&) usages are forbidden in this codebase. You must rule out your expressions being false!";
     private const string Category = "Usage";
     
     private static DiagnosticDescriptor Rule = new DiagnosticDescriptor(DiagnosticId, Title, MessageFormat, Category, DiagnosticSeverity.Error, isEnabledByDefault: true, description: Description);
