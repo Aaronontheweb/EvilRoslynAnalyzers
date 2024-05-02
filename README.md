13dca01e4ba4b9416fe1431744fd1aa613f5a417
Will Boulton <will@willboulton.com>
2024-05-02 03:27:10 +0100

    NoAndOperator analyzer (#35)
    * add analyzer which prohibits the use of the && operator
    
    * update readme with NoAndOperators description
    
    * update description

--- README.md
+++ README.md
@@ -4,3 +4,4 @@ Enterprise-approved insane C# Roslyn Analyzers under the motto: "don't let produ
 
 * [`NoExtensionMethods`](https://www.reddit.com/r/dotnet/comments/1c4hz1z/linq_forbidden/) - especially LINQ. If classes were supposed to have these methods, they would have implemented via inheritance obviously.
 * [`NoVarDeclarations`](https://twitter.com/michaeljolley/status/1782767354184667538) - the `var` keyword is dangerous; we should all write C# with explicit types in every expression, like a 1990s Java program.
+* [`NoAndOperators`](https://twitter.com/willboulton) - disallow usage of the `&&` operator, instead you must be explicit by ruling falsehood.
---
+++ src/EvilRoslynAnalyzers.Tests/BanAndTests.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.CodeAnalysis;
+using Verify = EvilRoslynAnalyzers.Tests.Utilities.EvilVerifier<EvilRoslynAnalyzers.NoAndOperatorAnalyzer>;
+
+namespace EvilRoslynAnalyzers.Tests;
+
+public class BanAndTests
+{
+    [Fact]
+    public void DemonstrateDeMorgansLaw()
+    {
+        bool DeMorgansLawAnd(bool a, bool b) => !(!a || !b);
+        
+        Assert.Equal(true && false, DeMorgansLawAnd(true, false));
+        Assert.Equal(false && true, DeMorgansLawAnd(false, true));
+        Assert.Equal(true && true, DeMorgansLawAnd(true, true));
+        Assert.Equal(false && false, DeMorgansLawAnd(false, false));
+    }
+    
+    
+    [Fact]
+    public Task ShouldNotDetectDeMorgansLawUsage()
+    {
+        string testCode = @"
+using System;
+public class TestClass {
+    public void TestMethod() {
+        bool a = true;
+        bool b = false;
+        if(!(!a || !b))
+        {
+        }
+    }
+}
+";
+        return Verify.VerifyAnalyzer(testCode);
+    }
+    
+    
+    [Fact]
+    public Task ShouldDetectAndOperatorUsage()
+    {
+        string testCode = @"
+using System;
+public class TestClass {
+    public void TestMethod() {
+        bool a = true;
+        bool b = false;
+        if(a && b)
+        {
+        }
+    }
+}
+";
+        return Verify.VerifyAnalyzer(testCode,Verify.Diagnostic().WithSpan(7, 12, 7, 18).WithSeverity(DiagnosticSeverity.Error));
+    }
+}
